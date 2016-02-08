e7a072f6e83b1edbe940c0d9516caf1a71cfa7dc
SRLarson <srltx@sbcglobal.net>
2016-02-08 15:56:14 -0600

    Q2 but actually Q3 complete

--- Homework 2/q2/src/PriorityQueue.java
+++ Homework 2/q2/src/PriorityQueue.java
@@ -6,26 +6,28 @@
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class PriorityQueue {
 	
-	private Semaphore mutex;
-	private Semaphore isFull;
-	private Semaphore isEmpty;
+	private ReentrantLock addLock, pollLock;
+	private Condition isFull, isEmpty;
 	private List<PriorityString> queue;	// Index ~ Priority
+	private AtomicInteger size;
 	private int capacity;
-	private int size;
     
 	// Creates a Priority queue with maximum allowed size as capacity	
 	public PriorityQueue(int maxSize) {
-		System.out.println("Capacity: " + maxSize);
-		this.mutex = new Semaphore(1);
-		this.isFull = new Semaphore(1);
-		this.isEmpty = new Semaphore(1);
+		this.addLock = new ReentrantLock();
+		this.pollLock = new ReentrantLock();
+		this.isEmpty = pollLock.newCondition();
+		this.isFull = addLock.newCondition();
+		
 		this.queue = new ArrayList<PriorityString>(maxSize);
+		this.size = new AtomicInteger(0);
 		this.capacity = maxSize;
-		this.size = 0;
 	}
 	
     // Adds the name with its priority to this queue.
@@ -33,20 +35,38 @@ public class PriorityQueue {
     // otherwise, returns -1 if the name is already present in the list.
     // This method blocks when the list is full.
 	public int add(String name, int priority) throws InterruptedException {
-		if (size >= capacity) {	//block
-			isFull.acquire();
+		int placement;
+		addLock.lock();
+		boolean wakePollers = false;
+		
+		try {
+			while(size.get() >= capacity) {
+				try {
+					isFull.await();
+				} catch (InterruptedException e) {}
+			}
+			
+			if(search(name) != -1) { throw new IllegalArgumentException(); }
+			
+			PriorityString newString = new PriorityString(name, priority);
+			placement = newString.findPriority(queue);
+			queue.add(placement, newString);
+			size.getAndIncrement();
+			wakePollers = true;
+		} catch (IllegalArgumentException e) {
+			placement = -1;
+		} finally {
+			addLock.unlock();
 		}
-//		mutex.acquire();		
 		
-		if(search(name) != -1) {return -1;}
-		
-		PriorityString newString = new PriorityString(name, priority);
-		int placement = newString.findPriority(queue);
-		queue.add(placement, newString);
-		size += 1;
-		
-//		mutex.release();
-		isEmpty.release();
+		if(wakePollers && placement != -1) {
+			pollLock.lock();
+			try {
+				isEmpty.signalAll();
+			} finally {
+				pollLock.unlock();
+			}
+		}
 		
 		return placement;
 	}
@@ -54,30 +74,42 @@ public class PriorityQueue {
     // Returns the position of the name in the list;
     // otherwise, returns -1 if the name is not found.
 	public int search(String name) throws InterruptedException {
-//		mutex.acquire();
-		
-		for(int i = 0; i < size; i++) {
+		for(int i = 0; i < size.get(); i++) {
 			if(queue.get(i).getName().equals(name)) { return i; }
 		}
-		
-//		mutex.release();
 		return -1;
 	}
 
     // Retrieves and removes the name with the highest priority in the list,
     // or blocks the thread if the list is empty.
 	public String poll() throws InterruptedException {
-//		mutex.acquire();
-		if(size <= 0) { //block
-			isEmpty.acquire();
+		pollLock.lock();
+		boolean wakeAdders = false;
+		PriorityString first;
+		
+		try {
+			while(size.get() <= 0) {
+				try {
+					isEmpty.await();
+				} catch (InterruptedException e) {}
+			}
+			
+			first = queue.get(size.get()-1);
+			queue.remove(size.get()-1);
+			size.getAndDecrement();
+			wakeAdders = true;
+		} finally {
+			pollLock.unlock();
 		}
 		
-		PriorityString first = queue.get(size-1);
-		queue.remove(size-1);
-		size -= 1;
-
-//		mutex.release();
-		isFull.release();
+		if(wakeAdders) {
+			addLock.lock();
+			try {
+				isFull.signalAll();
+			} finally {
+				addLock.unlock();
+			}
+		}
 		
 		return first.getName();
 	}
